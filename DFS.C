a528045abb22badfd263bd94a49c8a3a8bae634c
vibhahugar <117517944+vibhahugar@users.noreply.github.com>
2023-06-27 14:49:45 +0530

    Update DFS.C

--- DFS.C
+++ DFS.C
@@ -2,22 +2,28 @@
 #include<conio.h>
 
 void dfs(int);
-int n,i,j,a[10][10],vis[10];
+int i,j,n,a[10][10],vis[10];
 
 void main()
 {
-   clrscr();
+
    printf("enter the number of vertices\n");
    scanf("%d",&n);
    printf("enter the adjacency matrix\n");
+
    for(i=1;i<=n;i++)
    {
      for(j=1;j<=n;j++)
      {
        scanf("%d",&a[i][j]);
      }
+   }
+
+   for(i=1;i<=n;i++)
+   {
      vis[i]=0;
-    }
+   }
+
     printf("dfs traversal\n");
     for(i=1;i<=n;i++)
     {
@@ -27,15 +33,17 @@ void main()
     getch();
 }
 
+
 void dfs(int v)
 {
+   int i;
    vis[v]=1;
    printf("%d",v);
-   for(j=1;j<=n;j++)
+   for(i=1;i<=n;i++)
    {
-       if(a[v][j]==1 && vis[j]==0)
+       if(a[v][i]==1 && vis[i]==0)
        {
-	   dfs(j);
+	   dfs(i);
 	}
     }
 }
